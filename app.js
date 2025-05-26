3e79f0ac0bc3547e4a0e9b513409ef5e7e903e0e
Diana <160330162+HaterDi@users.noreply.github.com>
2025-05-26 16:04:27 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const restify = require('restify');
+const { BotFrameworkAdapter } = require('botbuilder');
+
+const adapter = new BotFrameworkAdapter({
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword
+});
+
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+  console.log(`\n[Bot] Слушает на ${server.url}`);
+});
+
+server.post('/api/messages', (req, res) => {
+  adapter.processActivity(req, res, async (context) => {
+    await context.sendActivity(`Вы сказали: ${context.activity.text}`);
+  });
+});
